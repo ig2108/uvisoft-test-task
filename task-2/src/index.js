027823a8a7efc1390723afb8bc83aaee0e623186
Ivan Golubey <ig2108@yahoo.com>
2020-10-20 00:59:04 +0300

    complete task 2, except image

---
+++ task-2/src/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+import contactList from './js/contactsList';
+
+import './styles.css';
+import './css/contacts-list.css';
+import './css/contacts-details.css';
+import './css/contact-info.css';
